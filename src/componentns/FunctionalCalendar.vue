69f403ca8df991a039cccccea7f7e8a9d354dec0
Manuk Minasyan <manuk.minasyan1@gmail.com>
2018-10-18 21:23:56 +0400

    Developed isModal Function

--- src/componentns/FunctionalCalendar.vue
+++ src/componentns/FunctionalCalendar.vue
@@ -1,40 +1,47 @@
 <template>
-  <div class="main-container">
-    <div class="wh_top_changge_buttons">
-      <li @click="PreMonth(myDate,false)">
-        <div class="wh_jiantou1"></div>
-      </li>
-      <li @click="NextMonth(myDate,false)">
-        <div class="wh_jiantou2"></div>
-      </li>
-    </div>
-    <div v-for="calendar in calendars.slice(0,calendarsCount)" class="calendar-for">
-      <section class="wh_container">
-        <div class="wh_content_all">
-          <div class="wh_top_changge">
-            <li class="wh_content_li">{{ calendar.dateTop }}</li>
-          </div>
-          <div class="wh_content">
-            <div class="wh_content_item" v-for="tag in textTop">
-              <div class="wh_top_tag">
-                {{tag}}
-              </div>
-            </div>
-          </div>
-          <div class="wh_content">
-            <div class="wh_content_item" v-for="(item,index) in calendar.list"
-                 @click="clickDay(item,index)">
-              <div class="wh_item_date"
-                   @mouseover="dateMouseOver(item.date)"
-                   v-bind:class="[getClassNames(item),setClass(item)]">
-                {{item.id}}
-              </div>
-            </div>
-          </div>
+    <div class="main-container">
+        <div class="input" v-if="isModal">
+            <input type="text" v-model="inputStartDate" @click="showCalendar = !showCalendar" readonly>
+            <input type="text" v-model="inputEndDate"   @click="showCalendar = !showCalendar" readonly>
         </div>
-      </section>
+
+        <div class="calendar" :class="{'modal': isModal}" v-if="showCalendar">
+            <div class="wh_top_changge_buttons">
+                <li @click="PreMonth(myDate,false)">
+                    <div class="wh_jiantou1"></div>
+                </li>
+                <li @click="NextMonth(myDate,false)">
+                    <div class="wh_jiantou2"></div>
+                </li>
+            </div>
+            <div v-for="calendar in calendars.slice(0,calendarsCount)" class="calendar-for">
+                <section class="wh_container">
+                    <div class="wh_content_all">
+                        <div class="wh_top_changge">
+                            <li class="wh_content_li">{{ calendar.dateTop }}</li>
+                        </div>
+                        <div class="wh_content">
+                            <div class="wh_content_item" v-for="tag in textTop">
+                                <div class="wh_top_tag">
+                                    {{tag}}
+                                </div>
+                            </div>
+                        </div>
+                        <div class="wh_content">
+                            <div class="wh_content_item" v-for="(item,index) in calendar.list"
+                                 @click="clickDay(item,index)">
+                                <div class="wh_item_date"
+                                     @mouseover="dateMouseOver(item.date)"
+                                     v-bind:class="[getClassNames(item),setClass(item)]">
+                                    {{item.id}}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </div>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
@@ -51,11 +58,19 @@
                     "July", "August", "September", "October", "November", "December"
                 ],
                 calendars: [],
+
+                showCalendar: true,
+                inputStartDate: 'yyyy/mm/dd',
+                inputEndDate: 'yyyy/mm/dd',
                 startDate: false,
                 endDate: false
             };
         },
         props: {
+            isModal: {
+                type: Boolean,
+                default: () => false
+            },
             isMultiple: {
                 type: Boolean,
                 default: () => false
@@ -90,9 +105,70 @@
             agoDayHide: {type: String, default: `0`},
             futureDayHide: {type: String, default: `2554387200`}
         },
+        watch: {
+            isModal: {
+                handler(val) {
+                    this.setIsModal(val)
+                }
+            },
+            startDate: {
+                handler(val) {
+                    this.inputStartDate = val ? val : 'yyyy/mm/dd';
+                }
+            },
+            endDate: {
+                handler(val) {
+                    this.inputEndDate = val ? val : 'yyyy/mm/dd';
+                }
+            },
+            markDate: {
+                handler() {
+                    this.listRendering(this.myDate);
+                },
+                deep: true
+            },
+            markDateMore: {
+                handler() {
+                    this.listRendering(this.myDate);
+                },
+                deep: true
+            },
+            agoDayHide: {
+                handler(val) {
+                    this.agoDayHide = parseInt(val);
+                    this.listRendering(this.myDate);
+                },
+                deep: true
+            },
+            futureDayHide: {
+                handler(val) {
+                    this.futureDayHide = parseInt(val);
+                    this.listRendering(this.myDate);
+                },
+                deep: true
+            },
+            sundayStart: {
+                handler() {
+                    this.intStart();
+                    this.listRendering(this.myDate);
+                }, deep: true
+            }
+        },
         created() {
             this.intStart();
             this.myDate = new Date();
+
+
+            let self = this;
+
+            window.addEventListener('click', function(e){
+                // close dropdown when clicked outside
+                if (!self.$el.contains(e.target)){
+                    self.showCalendar = false
+                }
+            })
+
+            this.setIsModal(this.isModal);
         },
         mounted() {
             this.listRendering(this.myDate);
@@ -321,232 +397,225 @@
                 }
 
                 this.calendars[calendar_index].list = arr;
-            }
-        },
-        watch: {
-            markDate: {
-                handler() {
-                    this.listRendering(this.myDate);
-                },
-                deep: true
             },
-            markDateMore: {
-                handler() {
-                    this.listRendering(this.myDate);
-                },
-                deep: true
-            },
-            agoDayHide: {
-                handler(val) {
-                    this.agoDayHide = parseInt(val);
-                    this.listRendering(this.myDate);
-                },
-                deep: true
-            },
-            futureDayHide: {
-                handler(val) {
-                    this.futureDayHide = parseInt(val);
-                    this.listRendering(this.myDate);
-                },
-                deep: true
-            },
-            sundayStart: {
-                handler() {
-                    this.intStart();
-                    this.listRendering(this.myDate);
-                }, deep: true
+            setIsModal(val = false) {
+                if(val) {
+                    this.showCalendar = false;
+                }
             }
         }
     };
 </script>
 
 <style scoped>
-  @media screen and (min-width: 460px) {
-    .wh_item_date:hover {
-      background-color: rgb(218, 218, 218);
-      border: 0;
-      border-radius: 50%;
-      cursor: pointer;
+    .calendar.modal{
+        margin-top: 5px;
+        will-change: transform, opacity;
+        position: relative;
+        background-color: #ffffff;
+        background-clip: padding-box;
+        box-shadow: 0 2px 15px 0 rgba(0,0,0,0.25);
+        transition: all 0.25s cubic-bezier(0.23, 1, 0.32, 1);
     }
 
-    .wh_item_date:hover.wh_isToday {
-      background-color: rgb(218, 218, 218);
-      border: 0;
-      border-radius: 50%;
-      cursor: pointer;
+    .input input {
+        margin-right: 0;
+        font-size: inherit;
+        transition: width 200ms;
+        padding-top: 5px;
+        padding-left: 5px;
+        padding-bottom: 5px;
+        width: 120px;
+        color: #aaa;
+        border: 1px solid #eee;
     }
-  }
 
-  .main-container {
-    width: -moz-fit-content;
-    width: fit-content;
-    margin: 50px 0 15px 0;
-  }
+    @media screen and (min-width: 460px) {
+        .wh_item_date:hover {
+            background-color: rgb(218, 218, 218);
+            border: 0;
+            border-radius: 50%;
+            cursor: pointer;
+        }
 
-  .wh_container {
-    display: flex;
-    max-width: 475px;
-    margin: auto;
-  }
+        .wh_item_date:hover.wh_isToday {
+            background-color: rgb(218, 218, 218);
+            border: 0;
+            border-radius: 50%;
+            cursor: pointer;
+        }
+    }
 
-  li {
-    list-style-type: none;
-  }
+    .main-container {
+        width: -moz-fit-content;
+        width: fit-content;
+        margin: 50px 0 15px 0;
+    }
 
-  .wh_top_changge {
-    display: flex;
-  }
+    .wh_container {
+        display: flex;
+        max-width: 475px;
+        margin: auto;
+    }
 
-  .wh_top_changge li {
-    cursor: pointer;
-    display: flex;
-    color: #000000;
-    font-size: 18px;
-    flex: 1;
-    justify-content: center;
-    align-items: center;
-    height: 47px;
-  }
+    li {
+        list-style-type: none;
+    }
 
-  .wh_top_changge .wh_content_li {
-    cursor: auto;
-    flex: 2.5;
-  }
+    .wh_top_changge {
+        display: flex;
+    }
 
-  .wh_top_changge_buttons {
-    display: flex;
-    width: 100%;
-    justify-content: space-between;
-    align-items: center;
-  }
+    .wh_top_changge li {
+        cursor: pointer;
+        display: flex;
+        color: #000000;
+        font-size: 18px;
+        flex: 1;
+        justify-content: center;
+        align-items: center;
+        height: 47px;
+    }
 
-  .wh_top_changge_buttons li {
-    cursor: pointer;
-    display: flex;
-    color: #000000;
-    font-size: 18px;
-    height: 47px;
-  }
+    .wh_top_changge .wh_content_li {
+        cursor: auto;
+        flex: 2.5;
+    }
 
-  .wh_top_changge_buttons .wh_content_li {
-    cursor: auto;
-  }
+    .wh_top_changge_buttons {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: 10px 20px 0;
+        margin-bottom: -25px;
+    }
 
-  .wh_content_all {
-    font-family: -apple-system, BlinkMacSystemFont, "PingFang SC",
-    "Helvetica Neue", STHeiti, "Microsoft Yahei", Tahoma, Simsun, sans-serif;
-    background-color: #FFFFFF;
-    width: 100%;
-    min-height: 377px;
-    overflow: hidden;
-    padding-bottom: 8px;
-  }
+    .wh_top_changge_buttons li {
+        cursor: pointer;
+        display: flex;
+        color: #000000;
+        font-size: 18px;
+    }
 
-  .wh_content {
-    display: flex;
-    flex-wrap: wrap;
-    padding: 0 3% 0 3%;
-    width: 100%;
-  }
+    .wh_top_changge_buttons .wh_content_li {
+        cursor: auto;
+    }
 
-  .wh_content:first-child .wh_content_item_tag,
-  .wh_content:first-child .wh_content_item {
-    color: #ddd;
-    font-size: 16px;
-  }
+    .wh_content_all {
+        font-family: -apple-system, BlinkMacSystemFont, "PingFang SC",
+        "Helvetica Neue", STHeiti, "Microsoft Yahei", Tahoma, Simsun, sans-serif;
+        background-color: #FFFFFF;
+        width: 100%;
+        min-height: 377px;
+        overflow: hidden;
+        padding-bottom: 8px;
+    }
 
-  .wh_content_item,
-  wh_content_item_tag {
-    font-size: 15px;
-    width: 13.4%;
-    text-align: center;
-    color: #000000;
-    position: relative;
-  }
+    .wh_content {
+        display: flex;
+        flex-wrap: wrap;
+        padding: 0 3% 0 3%;
+        width: 100%;
+    }
 
-  .wh_content_item {
-    height: 40px;
-    margin-bottom: 5px;
-  }
+    .wh_content:first-child .wh_content_item_tag,
+    .wh_content:first-child .wh_content_item {
+        color: #ddd;
+        font-size: 16px;
+    }
 
-  .wh_top_tag {
-    line-height: 40px;
-    margin: auto;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
+    .wh_content_item,
+    wh_content_item_tag {
+        font-size: 15px;
+        width: 13.4%;
+        text-align: center;
+        color: #000000;
+        position: relative;
+    }
 
-  .wh_item_date {
-    width: 40px;
-    height: 40px;
-    line-height: 40px;
-    margin: auto;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
+    .wh_content_item {
+        height: 40px;
+        margin-bottom: 5px;
+    }
 
-  .wh_jiantou1 {
-    width: 12px;
-    height: 12px;
-    border-top: 2px solid #0a0c19;
-    border-left: 2px solid #0a0c19;
-    transform: rotate(-45deg);
-  }
+    .wh_top_tag {
+        line-height: 40px;
+        margin: auto;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
 
-  .wh_jiantou1:active,
-  .wh_jiantou2:active {
-    border-color: #ddd;
-  }
+    .wh_item_date {
+        width: 40px;
+        height: 40px;
+        line-height: 40px;
+        margin: auto;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
 
-  .wh_jiantou2 {
-    width: 12px;
-    height: 12px;
-    border-top: 2px solid #0a0c19;
-    border-right: 2px solid #0a0c19;
-    transform: rotate(45deg);
-  }
+    .wh_jiantou1 {
+        width: 12px;
+        height: 12px;
+        border-top: 2px solid #0a0c19;
+        border-left: 2px solid #0a0c19;
+        transform: rotate(-45deg);
+    }
 
-  .wh_content_item > .wh_isMark {
-    margin: auto;
-    background-color: rgb(102, 179, 204);
-    border-width: 0px;
-    border-style: solid;
-    border-radius: 0px;
-    opacity: 1;
-    z-index: 2;
-  }
+    .wh_jiantou1:active,
+    .wh_jiantou2:active {
+        border-color: #ddd;
+    }
 
-  .wh_content_item .wh_other_dayhide {
-    color: #bfbfbf;
-  }
+    .wh_jiantou2 {
+        width: 12px;
+        height: 12px;
+        border-top: 2px solid #0a0c19;
+        border-right: 2px solid #0a0c19;
+        transform: rotate(45deg);
+    }
 
-  .wh_content_item .wh_other_dayhide.wh_isMark {
-    color: rgb(250, 250, 250);
-    background-color: rgba(102, 179, 204, 0.5);
-  }
+    .wh_content_item > .wh_isMark {
+        margin: auto;
+        background-color: rgb(102, 179, 204);
+        border-width: 0px;
+        border-style: solid;
+        border-radius: 0px;
+        opacity: 1;
+        z-index: 2;
+    }
 
-  .wh_content_item .wh_want_dayhide {
-    color: #bfbfbf;
-  }
+    .wh_content_item .wh_other_dayhide {
+        color: #bfbfbf;
+    }
 
-  .wh_content_item .wh_isToday {
-    background: #ff8080;
-    border-radius: 50%;
-  }
+    .wh_content_item .wh_other_dayhide.wh_isMark {
+        color: rgb(250, 250, 250);
+        background-color: rgba(102, 179, 204, 0.5);
+    }
 
-  .wh_content_item .wh_chose_day {
-    background-color: rgb(250, 250, 250);
-    border-color: rgb(102, 179, 204);
-    border-width: 2px;
-    border-style: solid;
-    border-radius: 50%;
-    opacity: 1;
-  }
+    .wh_content_item .wh_want_dayhide {
+        color: #bfbfbf;
+    }
 
-  .calendar-for {
-    display: inline-block;
-    margin-bottom: 5px;
-  }
+    .wh_content_item .wh_isToday {
+        background: #ff8080;
+        border-radius: 50%;
+    }
+
+    .wh_content_item .wh_chose_day {
+        background-color: rgb(250, 250, 250);
+        border-color: rgb(102, 179, 204);
+        border-width: 2px;
+        border-style: solid;
+        border-radius: 50%;
+        opacity: 1;
+    }
+
+    .calendar-for {
+        display: inline-block;
+        margin-bottom: 5px;
+    }
 </style>
--- src/main.js
+++ src/main.js
@@ -19,8 +19,9 @@ new Vue({
         // v-on:isToday="clickToday"
         // :sundayStart="true"
         // ':is-date-picker="true"'+
-        // ':is-multiple="true"'+
-        // ':calendars-count="4"'+
+        ':is-multiple="true"'+
+        ':is-modal="true"'+
+        // ':calendars-count="2"'+
         // ':markDate="arr"'+ // arr=['2018/4/1','2018/4/3'] Mark April 1st and April 3rd Simple Mark
 
         // ":agoDayHide='1539714127'"+ //Do not click before a date. Timestamp 10 digits
