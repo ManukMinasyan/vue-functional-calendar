935c3956bd2270c39b1e8cd6c4ec3694d14dc525
Manuk <manuk.minasyan1@gmail.com>
2018-11-16 00:37:51 +0400

    Vue.config.js changes

--- vue.config.js
+++ vue.config.js
@@ -1,18 +1,5 @@
-const path = require("path");
-
 module.exports = {
     runtimeCompiler: true,
-    baseUrl: '',
-	 outputDir: path.resolve(__dirname, "./dist"),
-	  chainWebpack: config => {
-		config.resolve.alias
-		  .set("@api", path.resolve(__dirname, "./src/api"));
-	  },
-
-  pluginOptions: {
-    quasar: {
-      theme: 'mat'
-    }
-  }
+    baseUrl: ''
 };
 
