e028140d2d7d91dd9ba1cf7b7c5634c7d22f8419
Manuk <manuk.minasyan1@gmail.com>
2018-11-16 00:27:28 +0400

    Build demo

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,18 @@
+const path = require("path");
+
 module.exports = {
     runtimeCompiler: true,
-    baseUrl: ''
+    baseUrl: '',
+	 outputDir: path.resolve(__dirname, "./dist"),
+	  chainWebpack: config => {
+		config.resolve.alias
+		  .set("@api", path.resolve(__dirname, "./src/api"));
+	  },
+
+  pluginOptions: {
+    quasar: {
+      theme: 'mat'
+    }
+  }
 };
+
