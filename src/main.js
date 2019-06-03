28e13b2c87da1465f3695f7e8f98aaf653de2580
Manuk <manuk.minasyan1@gmail.com>
2019-06-03 10:14:13 +0400

    Fix all conflicts

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import Vue from 'vue';
-import FunctionalCalendar from '.';
+import FunctionalCalendar from './components/FunctionalCalendar';
 import Demo from './Demo';
 
 Vue.use(FunctionalCalendar, {
