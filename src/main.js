c4da8715f1db8ac8219c82bc9c0a84786b10b187
Manuk Minasyan <manuk.minasyan1@gmail.com>
2019-12-15 01:09:18 +0400

    Fixed main.js conflict

--- src/main.js
+++ src/main.js
@@ -1,17 +1,9 @@
-<<<<<<< HEAD
-import Vue from 'vue';
-import FunctionalCalendar from '../index';
-import Demo from './Demo';
-
-Vue.use(FunctionalCalendar, {dayNames: ['Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa', 'Su']});
-=======
 import Vue from "vue";
 import FunctionalCalendar from "../index";
 import Demo from "./Demo";
 Vue.use(FunctionalCalendar, {
   dayNames: ["Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"]
 });
->>>>>>> c3d947c02d49b17d50f1fc268c051c365bb5be7e
 Vue.config.productionTip = false;
 
 new Vue({
